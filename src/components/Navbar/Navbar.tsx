c2154c55e5f01cb6aa4552164c1dea94ed7d920d
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-10 04:34:30 +0300

    created navbar frame

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import "./navbar.css"
+import React from "react";
+import logo from "../../assets/img/logo.png";
+import { IoIosCloseCircle, IoIosMenu } from "react-icons/io";
 
-type Props = {}
+import "./navbar.css";
+
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="header">
+        <div className="logoDiv">
+          <img src={logo} alt="rent2go-logo" className="logo" />
+        </div>
+      </div>
 
-export default Navbar
+      <div className="navbar">
+        <ul className="menu">
+          <li className="listItem">
+            <a href="/" className="link">
+              Used Cars
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="/" className="link">
+              New Cars
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="/" className="link">
+              Auctions
+            </a>
+          </li>
+          <li className="listItem">
+            <a href="/" className="link">
+              Sell
+            </a>
+          </li>
+        </ul>
+
+        <IoIosCloseCircle className="icon" />
+      </div>
+
+      <div className="signUp flex">
+        <div className="text">Sign Up</div>
+        <IoIosMenu className="icon" />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
