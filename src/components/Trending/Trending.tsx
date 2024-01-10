f992f91eff50f6a4dde3deb73fcd60ecf90d9d00
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-10 03:46:35 +0300

    created trending component

---
+++ src/components/Trending/Trending.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./trending.css"
+
+type Props = {}
+
+const Trending = (props: Props) => {
+  return (
+    <div>Trending</div>
+  )
+}
+
+export default Trending
