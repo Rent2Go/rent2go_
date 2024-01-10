5834e189de51aaffa3e3c4602247f0dc74eb5c32
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-10 03:30:02 +0300

    updated logo & content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/img/logo-dark.png";
 import "./App.css";
 import { Footer, Navbar } from "./components";
 import { Route, Routes } from "react-router-dom";
