0d40286f2e11441620a9aebf0b0de9ae8a33c6c4
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-10 03:56:01 +0300

    created project for admin dashboard

---
+++ admin/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo-dark.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Rent2Go Admin Dashboard
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
