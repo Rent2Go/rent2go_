86a1bb17c3a19bcdf0e57448d577fc2e1a2e69a7
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-12 01:47:42 +0300

    created search page & inserted input css

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import "./search.css"
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
-type Props = {}
+import "./search.css";
+
+type Props = {};
 
 const Search = (props: Props) => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="secContainer container">
+        <h3 className="title">Which vehicle you are looking for ? </h3>
 
-export default Search
+        <div className="searchDiv grid">
+          <input type="text" placeholder="Type" />
+          <input type="number" placeholder="Year" />
+          <input type="text" placeholder="Model" />
+          <input type="number" placeholder="Price" />
+          <button className="btn primaryBtn flex">
+            <AiOutlineSearch className="icon" />
+            <span>Search</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
