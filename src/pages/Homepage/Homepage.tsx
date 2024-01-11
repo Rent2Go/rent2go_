a367d3552cd197148e7a53b1f54497678beeebf7
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-12 01:48:45 +0300

    designed HomePage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import "./homepage.css"
+import React from "react";
+import "./homepage.css";
+import homeImage from "../../assets/img/carModels/toyota-corolla-beyaz.png"
 
-type Props = {}
+type Props = {};
 
 const Homepage = (props: Props) => {
   return (
-    <div></div>
-  )
-}
+    <div className="home">
+      <div className="secContainer">
+        <div className="homeText">
+          <span className="homeSpan">Meet your new Car</span>
+          <h1 className="homeTitle" >Honda Civic Type R</h1>
+          <div className="btns flex">
+            <button className="btn">More Details</button>
+            <button className="btn primaryBtn">Test Drive</button>
+          </div>
+        </div>
+        <div className="homeImage">
+          <img src={homeImage} alt="homeImage"/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
