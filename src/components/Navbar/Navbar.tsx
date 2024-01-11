9a7229bc0bdf6b6d3fe9cf05a805fe8e032a9f1e
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-12 01:45:49 +0300

    updated navbar media css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/img/logo.png";
 import { IoIosCloseCircle, IoIosMenu } from "react-icons/io";
 
@@ -7,48 +7,53 @@ import "./navbar.css";
 type Props = {};
 
 const Navbar = (props: Props) => {
+
+  const [navbar, setNavbar] = useState('navbar');
+
+  const showNavbar = () => {
+    setNavbar('navbar showNavbar')
+  }
+  const removeNavbar = () => {
+    setNavbar('navbar ')
+  }
   return (
     <>
       <div className="header">
         <div className="logoDiv">
           <img src={logo} alt="rent2go-logo" className="logo" />
         </div>
-        <div className="navbar">
-        <ul className="menu">
-          <li className="listItem">
-            <a href="/" className="link">
-              Used Cars
-            </a>
-          </li>
-          <li className="listItem">
-            <a href="/" className="link">
-              New Cars
-            </a>
-          </li>
-          <li className="listItem">
-            <a href="/" className="link">
-              Auctions
-            </a>
-          </li>
-          <li className="listItem">
-            <a href="/" className="link">
-              Sell
-            </a>
-          </li>
-        </ul>
+        <div className={navbar}>
+          <ul className="menu">
+            <li onClick={removeNavbar} className="listItem">
+              <a href="/" className="link">
+                Used Cars
+              </a>
+            </li>
+            <li onClick={removeNavbar} className="listItem">
+              <a href="/" className="link">
+                New Cars
+              </a>
+            </li>
+            <li onClick={removeNavbar} className="listItem">
+              <a href="/" className="link">
+                Auctions
+              </a>
+            </li>
+            <li onClick={removeNavbar} className="listItem">
+              <a href="/" className="link">
+                Sell
+              </a>
+            </li>
+          </ul>
 
-        <IoIosCloseCircle className="icon closeIcon" />
-      </div>
-
-      <div className="signUp flex">
-        <div className="text">
-          Sign Up 
+          <IoIosCloseCircle className="icon closeIcon" onClick={removeNavbar} />
         </div>
-        <IoIosMenu className="icon" />
-      </div>
-      </div>
 
-      
+        <div className="signUp flex">
+          <div className="text">Sign Up</div>
+          <IoIosMenu className="icon toggleNavbarIcon" onClick={showNavbar} />
+        </div>
+      </div>
     </>
   );
 };
