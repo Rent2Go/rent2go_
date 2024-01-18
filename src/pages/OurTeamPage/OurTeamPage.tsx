0b570a8a7cd0736a4f485a1f728fadd2408ccf28
Soner <syln.soner@gmail.com>
2024-01-18 21:00:47 +0300

    updated team page

--- src/pages/OurTeamPage/OurTeamPage.tsx
+++ src/pages/OurTeamPage/OurTeamPage.tsx
@@ -20,6 +20,7 @@ const OurTeamPage = (props: Props) => {
 
                     <div className="section-title">
                         <h2>Team</h2>
+                        <div className="underline"></div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, provident?</p>
                     </div>
 
