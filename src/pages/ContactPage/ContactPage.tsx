ff29a6dea38b265589a137252807c8dbe0375d11
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-19 16:39:39 +0300

    created basic contact information structure

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -7,6 +7,25 @@ const ContactPage = (props: Props) => {
   return (
     <>
       <Navbar />
+      <div className='contact'>
+        <div className='contact-form'>
+          <h1>Contact Us</h1>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div id='contact-container'>
+            <div className='contact-info'>
+
+            </div>
+            <form>
+              <h4>Contact Information</h4>
+              <p>text</p>
+              <div className='icon-text'>
+                <i className='icon'></i>
+                <span>text</span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
