f113e20f559695e75d3e0f3a2044fe07eed13e4f
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-21 05:36:34 +0300

    created sidebar, navLink dummyData and mapped on sidebar

--- admin/src/components/Sidebar/Sidebar.tsx
+++ admin/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import navLinks from "../../assets/dummy-data/NavLinks";
 
-type Props = {}
+import "./sidebar.css"
+
+type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h2>
+          <i className="ri-taxi-line"></i>Rent2go
+        </h2>
+      </div>
+      <div className="sidebar__content">
+        <div className="menu">
+          <ul className="nav__list">
+            {navLinks.map((item, index) => (
+              <li className="nav__item">
+                <NavLink to={item.path}>
+                  <span>
+                    <i className={item.icon}>{item.display}</i>
+                  </span>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
