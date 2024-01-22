bb6e291a5d663279046b2d4f646bacb3c3faa0e8
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-23 02:10:59 +0300

    creeted userCard & vehicleCard components and exported

---
+++ admin/src/components/Cards/UserCard/UserCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./UserCard.css"
+type Props = {}
+
+const UserCard = (props: Props) => {
+  return (
+    <div>UserCard</div>
+  )
+}
+
+export default UserCard
---
+++ admin/src/components/index.tsx
@@ -0,0 +1,1 @@
+export {default as UserCard} from "./Cards/UserCard/UserCard";
