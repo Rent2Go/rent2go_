fc7b1ae043e0f8c956f9736bb22bda9d032d99a6
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-23 02:20:03 +0300

    creeted navbar component and exported

--- admin/src/components/index.tsx
+++ admin/src/components/index.tsx
@@ -1,4 +1,7 @@
+export {default as Navbar} from "./Navbar/Navbar";
+export {default as Header} from "./Header/Header";
 export {default as Footer} from "./Footer/Footer";
+export {default as Layout} from "./Layout/Layout";
 
 
 export {default as UserCard} from "./Cards/UserCard/UserCard";
