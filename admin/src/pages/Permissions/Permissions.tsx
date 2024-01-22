27421b37287b1a03d4641d31b8fb418b7fbfb261
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-23 02:23:12 +0300

    created Permissions page and exported

---
+++ admin/src/pages/Permissions/Permissions.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./permissions.css"
+type Props = {}
+
+const Permissions = (props: Props) => {
+  return (
+    <div>Permissions</div>
+  )
+}
+
+export default Permissions
--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -1,1 +1,1 @@
-export{}
+export {default as Permissions} from "./Permissions/Permissions"
