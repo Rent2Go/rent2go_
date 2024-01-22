986d571fc1ed7f343b2ae521c14c13fa33effa4d
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-23 02:56:34 +0300

    created dashboard page

---
+++ admin/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./dashboard.css"
+type Props = {}
+
+const Dashboard = (props: Props) => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
