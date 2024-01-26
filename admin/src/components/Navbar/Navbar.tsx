2b1937a38e0eed0fb2315f4fa67e0db5c02b0a32
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-26 18:06:26 +0300

    created navbar

--- admin/src/components/Navbar/Navbar.tsx
+++ admin/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import "./navbar.css"
-type Props = {}
+import React from "react";
+import { IoMdSearch, IoIosNotifications } from "react-icons/io";
+
+import "./navbar.css";
+import { Link } from "react-router-dom";
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="navbar__wrapper">
+        <div className="search__box">
+          <input type="search" className="form-control" placeholder="search or type" />
+          <span>
+            <IoMdSearch />
+          </span>
+        </div>
+        <div className="navbar__right">
+          <span className="notification">
+            <IoIosNotifications />
+          </span>
+          <div className="profile">
+            <Link to=""><img className="img-rounded" src="assets/images/profile.png" alt="profile"/></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
