59ffa8dc404b0114f9e892bc96b0bb3fc14c33e4
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-27 21:47:17 +0300

    created in-page redirects in cookie policy

--- src/pages/LegalPages/CookiePolicy.tsx
+++ src/pages/LegalPages/CookiePolicy.tsx
@@ -12,11 +12,10 @@ const CookiePolicy = (props: Props) => {
           <div className="tabs_list">
             <ul>
               <li><a href='#item-1'>Interpretation and Definitions</a></li>
-              <li><a href="#item-2">Collecting and Using Your Personal Data</a></li>
-              <li><a href="#item-3">Children's Privacy</a></li>
-              <li><a href="#item-4">Links to Other Websites</a></li>
-              <li><a href="#item-5">Changes to this Privacy Policy</a></li>
-              <li><a href="#item-6">Contact Us</a></li>
+              <li><a href="#item-2">Type of Cookies We Use</a></li>
+              <li><a href="#item-3">Your Choices Regarding Cookies</a></li>
+              <li><a href="#item-4">More Information about Cookies</a></li>
+              <li><a href="#item-5">Contact Us</a></li>
             </ul>
           </div>
           <div className='tabs_content'>
@@ -27,20 +26,20 @@ const CookiePolicy = (props: Props) => {
               <p>Last updated: January 26, 2024</p>
               <p>This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used. This Cookies Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-cookies-policy-generator/" target="_blank">Free Cookies Policy Generator</a>.</p>
               <p>Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your personal data secure, see our Privacy Policy.</p>
-              <p>We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.</p>
+              <p id="item-1">We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.</p>
               <h2>Interpretation and Definitions</h2>
-              <h4>Interpretation</h4>
+              <h3>Interpretation</h3>
               <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
-              <h4>Definitions</h4>
+              <h3>Definitions</h3>
               <p>For the purposes of this Cookies Policy:</p>
               <ul>
                 <li><strong>Company</strong> (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Cookies Policy) refers to Rent2Go.</li>
                 <li><strong>Cookies</strong> means small files that are placed on Your computer, mobile device or any other device by a website, containing details of your browsing history on that website among its many uses.</li>
                 <li><strong>Website</strong> refers to Rent2Go, accessible from <a href="https://rentogo.com.tr/" rel="external nofollow noopener" target="_blank">https://rentogo.com.tr/</a></li>
-                <li><strong>You</strong> means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual is accessing or using the Website, as applicable.</li>
+                <li id="item-2"><strong>You</strong> means the individual accessing or using the Website, or a company, or any legal entity on behalf of which such individual is accessing or using the Website, as applicable.</li>
               </ul>
               <h2>The use of the Cookies</h2>
-              <h4>Type of Cookies We Use</h4>
+              <h3>Type of Cookies We Use</h3>
               <p>Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies. Persistent Cookies remain on your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close your web browser.</p>
               <p>We use both session and persistent Cookies for the purposes set out below:</p>
               <ul>
@@ -53,11 +52,11 @@ const CookiePolicy = (props: Props) => {
                 <li>
                   <p><strong>Functionality Cookies</strong></p>
                   <p>Type: Persistent Cookies</p>
-                  <p>Administered by: Us</p>
+                  <p id="item-3">Administered by: Us</p>
                   <p>Purpose: These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.</p>
                 </li>
               </ul>
-              <h4>Your Choices Regarding Cookies</h4>
+              <h3>Your Choices Regarding Cookies</h3>
               <p>If You prefer to avoid the use of Cookies on the Website, first You must disable the use of Cookies in your browser and then delete the Cookies saved in your browser associated with this website. You may use this option for preventing the use of Cookies at any time.</p>
               <p>If You do not accept Our Cookies, You may experience some inconvenience in your use of the Website and some features may not function properly.</p>
               <p>If You'd like to delete Cookies or instruct your web browser to delete or refuse Cookies, please visit the help pages of your web browser.</p>
@@ -75,10 +74,10 @@ const CookiePolicy = (props: Props) => {
                   <p>For the Safari web browser, please visit this page from Apple: <a href="https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac" rel="external nofollow noopener" target="_blank">https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac</a></p>
                 </li>
               </ul>
-              <p>For any other web browser, please visit your web browser's official web pages.</p>
-              <h4>More Information about Cookies</h4>
-              <p>You can learn more about cookies: <a href="https://www.freeprivacypolicy.com/blog/cookies/" target="_blank">Cookies: What Do They Do?</a>.</p>
-              <h4>Contact Us</h4>
+              <p id="item-4">For any other web browser, please visit your web browser's official web pages.</p>
+              <h2>More Information about Cookies</h2>
+              <p id="item-5">You can learn more about cookies: <a href="https://www.freeprivacypolicy.com/blog/cookies/" target="_blank">Cookies: What Do They Do?</a>.</p>
+              <h2>Contact Us</h2>
               <p>If you have any questions about this Cookies Policy, You can contact us:</p>
               <ul>
                 <li>By visiting this page on our website: <a href="https://rentogo.com.tr/contact" rel="external nofollow noopener" target="_blank">https://rentogo.com.tr/contact</a></li>
