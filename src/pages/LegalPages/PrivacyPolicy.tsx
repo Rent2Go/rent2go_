0364eab485b03843e3e297dd34c92b1baee54965
Yagmur Curku <yagmurcurku@gmail.com>
2024-01-27 20:43:16 +0300

    updated privacyPolicy

--- src/pages/LegalPages/PrivacyPolicy.tsx
+++ src/pages/LegalPages/PrivacyPolicy.tsx
@@ -29,8 +29,8 @@ const PrivacyPolicy = (props: Props) => {
           <div className='tab_body'>
             <p>Last updated: January 26, 2024</p>
             <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-            <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank">Free Privacy Policy Generator</a>.</p>
-            <h2 id='item-1'>Interpretation and Definitions</h2>
+            <p id='item-1'>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank">Free Privacy Policy Generator</a>.</p>
+            <h2>Interpretation and Definitions</h2>
             <h3>Interpretation</h3>
             <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
             <h3>Definitions</h3>
@@ -70,10 +70,10 @@ const PrivacyPolicy = (props: Props) => {
                 <p><strong>Website</strong> refers to Rent2Go, accessible from <a href="https://rentogo.com.tr/" rel="external nofollow noopener" target="_blank">https://rentogo.com.tr/</a></p>
               </li>
               <li>
-                <p><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
+                <p id='item-2'><strong>You</strong> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</p>
               </li>
             </ul>
-            <h2 id='item-2'>Collecting and Using Your Personal Data</h2>
+            <h2>Collecting and Using Your Personal Data</h2>
             <h3>Types of Data Collected</h3>
             <h4>Personal Data</h4>
             <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information that can be used to contact or identify You. Personally identifiable information may include, but is not limited to:</p>
@@ -189,18 +189,18 @@ const PrivacyPolicy = (props: Props) => {
               <li>Protect against legal liability</li>
             </ul>
             <h3>Security of Your Personal Data</h3>
-            <p>The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
-            <h2 id='item-3'>Children's Privacy</h2>
+            <p id='item-3'>The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
+            <h2>Children's Privacy</h2>
             <p>Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.</p>
-            <p>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</p>
-            <h2 id='item-4'>Links to Other Websites</h2>
+            <p id='item-4'>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</p>
+            <h2>Links to Other Websites</h2>
             <p>Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</p>
-            <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
-            <h2 id='item-5'>Changes to this Privacy Policy</h2>
+            <p id='item-5'>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+            <h2>Changes to this Privacy Policy</h2>
             <p>We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.</p>
             <p>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the &quot;Last updated&quot; date at the top of this Privacy Policy.</p>
-            <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
-            <h2 id='item-6'>Contact Us</h2>
+            <p id='item-6'>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+            <h2>Contact Us</h2>
             <p>If you have any questions about this Privacy Policy, You can contact us:</p>
             <ul>
               <li>By visiting this page on our website: <a href="https://rentogo.com.tr/contact" rel="external nofollow noopener" target="_blank">https://rentogo.com.tr/contact</a></li>
