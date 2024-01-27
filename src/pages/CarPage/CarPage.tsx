baf794f68ce18c2e83ce0cb965b111168e0106c8
ShmsErl <erol.sehmus1@gmail.com>
2024-01-28 00:52:57 +0300

    Created request results for getAll request.(then and catch).

--- src/pages/CarPage/CarPage.tsx
+++ src/pages/CarPage/CarPage.tsx
@@ -29,14 +29,21 @@ const CarPage: React.FC<Props> = (props) => {
   const carsState = useSelector((state: any) => state.car);
 
   const getCars = async () => {
-    try {
-     
-      const response = await CarService.getAll();
-      setCars(response.data.data);
-      setIsInitialLoad(false);
-    } catch (error) {
-      console.error("Error fetching cars:", error);
-    }
+
+
+    const response = await CarService.getAll()
+      .then((res) => {
+        setCars(res.data.data);
+        setIsInitialLoad(false);
+
+      }
+      )
+      .catch((err) => {
+        console.log(err);
+      })
+
+
+
   };
 
   useEffect(() => {
