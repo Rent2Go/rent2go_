666d4ffe57b6d45de1d6744d44e84591897db906
ShmsErl <erol.sehmus1@gmail.com>
2024-01-29 17:28:36 +0300

    The list of tools has been moved to the store structure with the redux toolkit.

--- src/store/slices/carSlice.ts
+++ src/store/slices/carSlice.ts
@@ -1,18 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { CarModel } from "../../models/responses/cars/GetCar";
+import CarService from "../../services/CarService";
 
-interface CarItem{
-    car: CarModel;
-}
+
+
+export const fetchCarData = createAsyncThunk("fetchCarData", async (args, thunkAPI) => {
+
+  const state: any = thunkAPI.getState();
+  console.log(state.car.cars > 0);
+  console.log( !(new Date().getTime() - state.car.lastFetch > 60000));
+  
+  if (
+    state.car.cars > 0
+  ) {
+    return state.car.cars;
+  }
+
+  const response = await CarService.getAll();
+
+  return response.data.data;
+ /* &&
+  !(new Date().getTime() - state.car.lastFetch > 60000)
+*/
+});
 
 const carSlice = createSlice({
-    name: "car",
-    initialState: {
-      carItems: (JSON.parse(localStorage.getItem("cart")!) || []) as CarItem[],
-    },
-    reducers:{
+  name: "cars",
+  initialState: { loading: "", cars: [] as CarModel[], lastFetch: new Date().getTime() },
+  reducers: {},
 
-    }
+  extraReducers: builder => {
+    builder.addCase(fetchCarData.pending, state => {
+      state.loading = "Loading cars"
+    });
+    builder.addCase(fetchCarData.fulfilled, (state, action) => {
+      state.loading = "loaded";
+      state.cars = action.payload;
+    });
+    builder.addCase(fetchCarData.rejected, state => {
+      state.loading = "Errors"
+    });
+
+  }
 })
 
 export const carReducer = carSlice.reducer;
