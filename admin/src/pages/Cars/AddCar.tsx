23a5eb5dccb76d54dd622cf1baef042c6ee11580
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-30 23:25:37 +0300

    completed Add Car

--- admin/src/pages/Cars/AddCar.tsx
+++ admin/src/pages/Cars/AddCar.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/cars.css";
 import { Field, Form, Formik } from "formik";
 import { FormikInput, FormikSelect } from "../../components";
+import { useDropzone } from "react-dropzone";
+import { CarModel } from "../../models/responses/cars/GetCar";
+import CarService from "../../services/CarService";
+import "react-dropzone-uploader/dist/styles.css";
+import Dropzone from "react-dropzone-uploader";
+import { Link } from "react-router-dom";
 type Props = {};
 
 const AddCar = (props: Props) => {
   const initialValues = () => {};
   const onSubmit = () => {};
+
+  const getUploadParams = ({}) => {
+    return { url: "https://httpbin.org/post" };
+  };
+
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({}) => {
+    console.log();
+  };
+
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = () => {};
+
   return (
     <div className="cars container">
+      <div className="secContainer">
       <div className="headerContainer">
         <h2>Add New Car</h2>
       </div>
@@ -28,9 +48,105 @@ const AddCar = (props: Props) => {
                 <FormikSelect id="1" name="color" label="Color"></FormikSelect>
               </div>
             </div>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikSelect id="2" name="Brand" label="Brand"></FormikSelect>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikSelect id="3" name="Model" label="Model"></FormikSelect>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="year"
+                  type="text"
+                  label="Year"
+                  placeHolder="Enter Model Year"
+                ></FormikInput>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikSelect
+                  id="4"
+                  name="bodyType"
+                  label="Body Type"
+                ></FormikSelect>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikSelect
+                  id="2"
+                  name="fuelType"
+                  label="Fuel Type"
+                ></FormikSelect>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikSelect
+                  id="3"
+                  name="gearType"
+                  label="Gear Type"
+                ></FormikSelect>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="cylinderCount"
+                  type="text"
+                  label="Cylinder Count"
+                  placeHolder="Enter Cylinder Count"
+                ></FormikInput>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="enginePower"
+                  type="text"
+                  label="Engine Power"
+                  placeHolder="Enter Engine Power"
+                ></FormikInput>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="kilometer"
+                  type="text"
+                  label="Kilometer"
+                  placeHolder="Enter Kilometer"
+                ></FormikInput>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="dailyPrice"
+                  type="text"
+                  label="Daily Price"
+                  placeHolder="Enter Daily Price"
+                ></FormikInput>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-12 col-l-12 col-md-12 col-sm-12">
+                <label className="mb-3 mt-5">Images</label>
+                <Dropzone
+                  getUploadParams={getUploadParams}
+                  onChangeStatus={handleChangeStatus}
+                  onSubmit={handleSubmit}
+                  accept="image/*"
+                />
+              </div>
+            </div>
+            <div className="btnContainer">
+              <button title="Save" type="submit" className="btn btn-sm btn-submit">Save</button>
+              <Link to="/cars" className="btn btn-sm btn-cancel">Cancel</Link>
+            </div>
           </Form>
         </Formik>
       </div>
+      </div>
+
+      
+     
     </div>
   );
 };
