8bee360344ada559e7f0a8045db1f5ff2d01d5cb
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-01-30 21:26:35 +0300

    modified add car page

--- admin/src/pages/Cars/AddCar.tsx
+++ admin/src/pages/Cars/AddCar.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import "./styles/cars.css"
-type Props = {}
+import React from "react";
+import "./styles/cars.css";
+import { Field, Form, Formik } from "formik";
+import { FormikInput, FormikSelect } from "../../components";
+type Props = {};
 
 const AddCar = (props: Props) => {
+  const initialValues = () => {};
+  const onSubmit = () => {};
   return (
-    <div>AddCar</div>
-  )
-}
+    <div className="cars container">
+      <div className="headerContainer">
+        <h2>Add New Car</h2>
+      </div>
+      <div className="formContainer">
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          <Form>
+            <div className="row">
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+                <FormikInput
+                  name="plate"
+                  type="text"
+                  label="Plate No"
+                  placeHolder="Enter Plate No"
+                ></FormikInput>
+              </div>
+              <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
+              <FormikSelect
+                  name="plate"
+                  label="Plate No"
+                ></FormikSelect>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
 
-export default AddCar
+export default AddCar;
