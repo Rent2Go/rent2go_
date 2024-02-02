37e646e8f2ca0d470c93a8acb05af522fbd329f7
ShmsErl <erol.sehmus1@gmail.com>
2024-02-02 22:31:39 +0300

    Created FilterReducer for filter cars.

--- src/store/slices/carSlice.ts
+++ src/store/slices/carSlice.ts
@@ -7,18 +7,15 @@ import CarService from "../../services/CarService";
 export const fetchCarData = createAsyncThunk("fetchCarData", async (args, thunkAPI) => {
 
   const state: any = thunkAPI.getState();
-      console.log(new Date().getTime() - state.car.lastFetch);
-  if (state.car.cars.length  > 0 && !( new Date().getTime() - state.car.lastFetch > 1000 * 60 * 60 ) ) {
- 
-    
-  
+
+  if (state.car.cars.length > 0 && !(new Date().getTime() - state.car.lastFetch > 1000 * 60 * 60)) {
     return state.car.cars;
   }
 
   const response = await CarService.getAll();
 
   return response.data.data;
- 
+
 });
 
 const carSlice = createSlice({
@@ -29,18 +26,17 @@ const carSlice = createSlice({
   extraReducers: builder => {
     builder.addCase(fetchCarData.pending, state => {
       state.loading = "Loading cars"
-      console.log("1");
-      
+
+
     });
     builder.addCase(fetchCarData.fulfilled, (state, action) => {
       state.loading = "loaded";
       state.cars = action.payload;
-      console.log("12");
-      console.log(state.cars);
+
     });
     builder.addCase(fetchCarData.rejected, state => {
       state.loading = "Errors"
-      console.log("123");
+
     });
 
   }
