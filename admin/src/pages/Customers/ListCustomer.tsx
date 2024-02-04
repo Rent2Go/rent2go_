86d97b041e1966f7cab18f92fafc8519f7a60c57
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-04 21:18:05 +0300

    init customer table into customer list page

--- admin/src/pages/Customers/ListCustomer.tsx
+++ admin/src/pages/Customers/ListCustomer.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./styles/customers.css"
+import { CustomerTable } from '../../components'
+import { Link } from 'react-router-dom'
+import { IoMdAdd } from 'react-icons/io'
 
 type Props = {}
 
 const ListCustomer = (props: Props) => {
   return (
-    <div>ListCustomer</div>
+    <div className='customers'>
+      <div className="headingContainer">
+        <h2>Customers</h2>
+        <div className="addBtnContainer">
+          <Link title="Add New User" to="/add-customer" className="btn btn-sm">
+            <IoMdAdd /> Add Customer
+          </Link>
+        </div>
+      </div>
+      <div className="secContainer">
+        <div className="customerContainer">
+          <CustomerTable />
+        </div>
+      </div>
+    </div>
   )
 }
 
