1411a548624d42f516dc4dcb354ec2ace57a064b
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-04 22:02:00 +0300

    created discount page

---
+++ admin/src/pages/Discounts/Discounts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import "./discounts.css"
+type Props = {}
+
+const Discounts = (props: Props) => {
+  return (
+    <div>Discounts</div>
+  )
+}
+
+export default Discounts
