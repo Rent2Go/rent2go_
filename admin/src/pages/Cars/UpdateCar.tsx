21daa1976da16ebcfd8f663177704b9f412ab712
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-04 03:32:13 +0300

    added models to UpdateCar.tsx

--- admin/src/pages/Cars/UpdateCar.tsx
+++ admin/src/pages/Cars/UpdateCar.tsx
@@ -10,6 +10,8 @@ import { ColorModel } from '../../models/responses/colors/ColorModel';
 import ColorService from '../../services/ColorService';
 import { BrandModel } from '../../models/responses/brands/GetBrand';
 import BrandService from '../../services/BrandService';
+import ModelService from '../../services/ModelService';
+import { ModelModel } from '../../models/responses/models/GetModel';
 
 type Props = {}
 
@@ -20,11 +22,13 @@ const UpdateCar = (props: Props) => {
 
   const [colors, setColors] = useState<ColorModel[]>([]);
   const [brands, setBrands] = useState<BrandModel[]>([]);
+  const [models, setModels] = useState<ModelModel[]>([]);
 
 
   useEffect( () => {
     getColors()
     getBrands()
+    getModels()
   },[])
 
   const getCarDetails = async (id: string) => {
@@ -59,6 +63,12 @@ const UpdateCar = (props: Props) => {
     .catch( (err) => {console.log(err)} )
   }
 
+  const getModels = () => {
+    const response = ModelService.getAll()
+    .then( (res) => {setModels(res.data.data)} )
+    .catch( (err) => {console.log(err)} )
+  }
+
   return (
     <div className="cars container">
     <div className="secContainer">
@@ -87,7 +97,7 @@ const UpdateCar = (props: Props) => {
               <FormikSelect id="2" name="Brand" label="Brand" colors={brands}></FormikSelect>
             </div>
             <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
-              <FormikSelect id="3" name="Model" label="Model" colors={[]}></FormikSelect>
+              <FormikSelect id="3" name="Model" label="Model" colors={models}></FormikSelect>
             </div>
           </div>
           <div className="row">
