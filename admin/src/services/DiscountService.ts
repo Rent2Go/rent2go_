481a482786191c2b4061bc0a6e1136a1b7c7db8b
Soner <syln.soner@gmail.com>
2024-02-06 23:10:40 +0300

    Add discount form and functionality

--- admin/src/services/DiscountService.ts
+++ admin/src/services/DiscountService.ts
@@ -1,13 +1,18 @@
 import { GetAllDiscountModel } from "../models/responses/discounts/GetAllDiscounts";
 import { DiscountModel } from "../models/responses/discounts/GetDiscount";
+import { AddDiscountRequest } from "../models/requests/discounts/AddDiscountRequest";
 import axiosInstance from "../utils/axiosInsterceptors";
+import { AddResponse } from "../../../src/models/responses/AddResponse";
 
 class DiscountService{
     getAll(){
         return axiosInstance.get<GetAllDiscountModel>("discounts")
     }
     getById(id:number){
-        return axiosInstance.get<DiscountModel>(`discount/${id}`)
+        return axiosInstance.get<DiscountModel>(`discounts/${id}`)
+    }
+    create(data:AddDiscountRequest){
+        return axiosInstance.post<AddResponse>("discounts",data)
     }
 }
 
