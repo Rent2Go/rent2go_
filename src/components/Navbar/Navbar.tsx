56610f00743ed0c0ac76539d55b704658cea21d0
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-07 03:25:50 +0300

    created language btns into navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { hasFlag } from "country-flag-icons";
+import { TR, US } from "country-flag-icons/react/3x2";
 import { IoIosCloseCircle, IoIosMenu } from "react-icons/io";
 import { IoMdCart } from "react-icons/io";
 import { NavDropdown } from "react-bootstrap";
@@ -6,7 +9,6 @@ import Image from "react-bootstrap/Image";
 import { FaUserCircle } from "react-icons/fa";
 import { IoMdLogIn, IoMdLogOut } from "react-icons/io";
 import Nav from "react-bootstrap/Nav";
-
 
 import "./navbar.css";
 import { Link } from "react-router-dom";
@@ -17,6 +19,10 @@ import TokenService from "../../services/TokenService";
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
   const [navbar, setNavbar] = useState("navbar");
 
   const showNavbar = () => {
@@ -36,17 +42,21 @@ const Navbar = (props: Props) => {
   const logout = () => {
     TokenService.removeToken("token");
     TokenService.removerefreshToken("refreshToken");
-    toast.success("Successful Exit")
-  }
+    toast.success("Successful Exit");
+  };
 
   window.addEventListener("scroll", addBg);
-  const authContext: any = useAuth()
+  const authContext: any = useAuth();
   return (
     <>
       <div className={header}>
         <div className="logoDiv">
           <Link to="/" className="link">
-            <img src="assets/img/logo.png" alt="rent2go-logo" className="logo" />
+            <img
+              src="assets/img/logo.png"
+              alt="rent2go-logo"
+              className="logo"
+            />
           </Link>
         </div>
         <div className={navbar}>
@@ -77,6 +87,8 @@ const Navbar = (props: Props) => {
         </div>
 
         <div className="signUp flex">
+       
+
           {authContext.authInformation.isAuthenticated ? (
             <>
               <NavDropdown
@@ -90,14 +102,17 @@ const Navbar = (props: Props) => {
               >
                 <Link className=" text textAction btn" to="/profile">
                   {" "}
-                 
                   Profile
                 </Link>
 
                 <Link className="  btn text textAction" to="/cart">
                   Reservation
                 </Link>
-                <Link className="text textAction btn" to="/sign-up" onClick={logout}>
+                <Link
+                  className="text textAction btn"
+                  to="/sign-up"
+                  onClick={logout}
+                >
                   Log Out
                 </Link>
               </NavDropdown>
@@ -109,6 +124,26 @@ const Navbar = (props: Props) => {
               </Link>
             </>
           )}
+             <div className="languageBtnContainer">
+            <button
+              name="en"
+              type="button"
+              className="langButton"
+              onClick={() => changeLanguage("en")}
+            >
+              {" "}
+              <US title="English" />
+            </button>
+            <button
+              name="tr"
+              type="button"
+              className="langButton"
+              onClick={() => changeLanguage("tr")}
+            >
+              {" "}
+              <TR title="Turkish" />
+            </button>
+          </div>
           <IoIosMenu className="icon toggleNavbarIcon" onClick={showNavbar} />
         </div>
       </div>
