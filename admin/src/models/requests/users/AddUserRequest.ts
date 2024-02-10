ab672911ef6347b405b8c8685985269c104c7aa4
ShmsErl <erol.sehmus1@gmail.com>
2024-02-10 11:26:06 +0300

    UserRequest modelling was done.

---
+++ admin/src/models/requests/users/AddUserRequest.ts
@@ -0,0 +1,17 @@
+
+export interface AddUserRequest {
+
+  
+	name: string;
+	surname: string;
+	phoneNumber: string;
+	email: string;
+	password: string;
+    confirmPassword: string;
+    role: string;
+	
+} 
+
+
+
+
