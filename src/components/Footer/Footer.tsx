2c61745446b3cb1a8e3eca978ae8d642c9b39f1b
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-10 03:54:02 +0300

    added translation to footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import "./footer.css";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 type Props = {};
 
 const Footer = (props: Props) => {
+  const {t} = useTranslation();
   return (
     <div className="footer">
       <div className="footerContainer container">
         <div className="footerMenuDiv grid">
           <div className="singleGrid" data-aos="fade-up" data-aos-duration="1000">
-            <span className="footerTitle">About</span>
+            <span className="footerTitle">{t("about")}</span>
             <ul className="footerUl grid">
-              <li className="footerLi"><Link to="/our-team">Who we are</Link></li>
-              <li className="footerLi"><Link to="https://tobeto.com/istanbul-kodluyor">Careers</Link></li>
-              <li className="footerLi"><Link to="https://www.istka.org.tr/">Affiliates</Link></li>
-              <li className="footerLi"><Link to="coming-soon">Media</Link></li>
+              <li className="footerLi"><Link to="/our-team">{t("whoWeAre")}</Link></li>
+              <li className="footerLi"><Link to="https://tobeto.com/istanbul-kodluyor">{t("careers")}</Link></li>
+              <li className="footerLi"><Link to="https://www.istka.org.tr/">{t("affiliates")}</Link></li>
+              <li className="footerLi"><Link to="coming-soon">{t("media")}</Link></li>
             </ul>
           </div>
 
 
           <div className="singleGrid" data-aos="fade-up" data-aos-duration="1600">
-            <span className="footerTitle">Relational Pages</span>
+            <span className="footerTitle">{t("relationalPages")}</span>
             <ul className="footerUl grid">
-              <li className="footerLi"><Link to="https://tobeto.com/istanbul-kodluyor">İstanbul Kodluyor</Link></li>
+              <li className="footerLi"><Link to="https://tobeto.com/istanbul-kodluyor">{t("istanbulKodluyor")}</Link></li>
               <li className="footerLi"><Link to="https://tobeto.com/">Tobeto</Link></li>
               <li className="footerLi"><Link to="https://kodlama.io/">Kodlama.io</Link></li>
               <li className="footerLi"><Link to="https://www.enocta.com/">Enocta</Link></li>
@@ -32,19 +35,19 @@ const Footer = (props: Props) => {
           </div>
 
           <div className="singleGrid" data-aos="fade-up" data-aos-duration="1800">
-            <span className="footerTitle">Booking Support</span>
+            <span className="footerTitle">{t("bookingSupport")}</span>
             <ul className="footerUl grid">
-              <li className="footerLi"><Link to="/contact">Contact Us</Link></li>
-              <li className="footerLi"><Link to="/cookie-policy">Cookies</Link></li>
-              <li className="footerLi"><Link to="/privacy-policy">Privacy Policy</Link></li>
-              <li className="footerLi"><Link to="/terms-of-use">Terms of Use</Link></li>
+              <li className="footerLi"><Link to="/contact">{t("contactUs")}</Link></li>
+              <li className="footerLi"><Link to="/cookie-policy">{t("cookies")}</Link></li>
+              <li className="footerLi"><Link to="/privacy-policy">{t("privacyPolicy")}</Link></li>
+              <li className="footerLi"><Link to="/terms-of-use">{t("termsOfUse")}</Link></li>
             </ul>
           </div>
         </div>
       </div>
       <div className="lowerSection grid" data-aos="fade-up" data-aos-duration="1200">
-        <p> © {currentYear} All rights reserved by </p>
-        <blockquote>Rent2Go Projects</blockquote>
+        <p> © {currentYear} {t("allRightsReservedBy")} </p>
+        <blockquote>{t("rent2GoProjects")}</blockquote>
       </div>
     </div>
   );
