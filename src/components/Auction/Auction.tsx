2eb7719b0b7fb90374cba5c7f497bfb4a4f315e3
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-10 03:53:11 +0300

    added translation to auction component in homepage

--- src/components/Auction/Auction.tsx
+++ src/components/Auction/Auction.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useTranslation } from "react-i18next";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 
 import './auctions.css';
@@ -7,11 +7,13 @@ import './auctions.css';
 type Props = {};
 
 const Action = (props: Props) => {
+  const {t} = useTranslation();
+
   return (
     <div className="auction section">
       <div className="secContainer container">
         <div className="secHeading flex" data-aos="fade-up" data-aos-duration="2000">
-          <h3 className="secTitle" >Active Auctions</h3>
+          <h3 className="secTitle">{t("activeAuctions")}</h3>
           <div className="navBtns flex">
             <BsArrowLeftShort className="icon leftIcon" />
             <BsArrowRightShort className="icon rightIcon" />
@@ -23,12 +25,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/mercedesc200-beyaz.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
           <div className="singleCar grid singleCarActive" data-aos="fade-up" data-aos-duration="5000">
@@ -36,12 +38,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/peugeot-2008-siyah.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
           <div className="singleCar grid" data-aos="fade-up" data-aos-duration="5000">
@@ -49,12 +51,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/minicooper-s-mavi.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
           <div className="singleCar grid singleCarActive" data-aos="fade-up" data-aos-duration="5000">
@@ -62,12 +64,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/renault-clio-beyaz.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
           <div className="singleCar grid" data-aos="fade-up" data-aos-duration="5000">
@@ -75,12 +77,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/renault-megane-gri.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
           <div className="singleCar grid singleCarActive" data-aos="fade-up" data-aos-duration="5000">
@@ -88,12 +90,12 @@ const Action = (props: Props) => {
               <img src="assets/img/carModels/renault-clio-kırmızı.png" alt="carImage" />
             </div>
             <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">11457 Miles</span>
+            <span className="miles">{`11457 ${t("miles")}`}</span>
             <span className="AWD">AWD 4-Cylinder Turbo</span>
 
             <div className="price_buyBtn flex">
               <span className="price">$41,200</span>
-              <span className="buyBtn">Buy Now</span>
+              <span className="buyBtn">{t("rentNow")}</span>
             </div>
           </div>
         </div>
