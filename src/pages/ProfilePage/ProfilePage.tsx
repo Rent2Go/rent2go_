71e9c8cbabb9b66ceb344a1f45f9887ee41d607d
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-12 16:54:26 +0300

    created right and left div structure of the profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Footer, Navbar } from '../../components'
+import { useParams } from 'react-router-dom'
+import AccountSettings from '../../components/UserProfile/AccountSettings/AccountSettings'
+import UserSidebar from '../../components/UserProfile/UserSidebar/UserSidebar'
+
+
 
 type Props = {}
 
 const ProfilePage = (props: Props) => {
+
+  const {activepage} = useParams();
+
   return (
     <>
       <Navbar />
       ProfilePage
-      <div className='left'></div>
-      <div className='right'></div>
+      <div className='userProfileIn'>
+        <div className='left'>
+          <UserSidebar activepage={activepage} />
+        </div>
+        <div className='right'>
+          {activepage === 'accountsettings' && <AccountSettings />}
+        </div>
+      </div>
       <Footer />
     </>
   )
