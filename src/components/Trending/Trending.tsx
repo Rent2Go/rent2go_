01bb1b0f4df0e881d9bf6deb08c4d4ba9905cfcc
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-13 22:58:29 +0300

    modified trending component

--- src/components/Trending/Trending.tsx
+++ src/components/Trending/Trending.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsArrowLeftShort, BsArrowRightShort } from "react-icons/bs";
 import { useTranslation } from "react-i18next";
+import { FaStar } from "react-icons/fa";
+import { CiDiscount1 } from "react-icons/ci";
 
 import "./trending.css";
 
@@ -28,13 +30,13 @@ const Trending = (props: Props) => {
             >
               <img src="assets/img/carModels/maserati-quattroporte-mavi.png" alt="carImage" />
             </div>
-            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">{`11457 ${t("miles")}`}</span>
-            <span className="AWD">AWD 4-Cylinder Turbo</span>
+            <h5 className="carTitle">Maserati Quattroporte 2008</h5>
+            <span className="miles">{`83492 ${t("kilometer")}`}</span>
+            <span className="AWD">AWD 400 hp Turbo</span>
 
             <div className="price_seller flex">
-              <span className="price">$41,200</span>
-              <span className="seller">{t("bestSeller")}</span>
+              <span className="price"><CiDiscount1 /></span>
+              <span className="seller"><FaStar /></span>
             </div>
           </div>
 
@@ -42,13 +44,13 @@ const Trending = (props: Props) => {
             <div className="imgDiv" data-aos="fade-up" data-aos-duration="2500">
               <img src="assets/img/carModels/mercedesc200-beyaz.png" alt="carImage" />
             </div>
-            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">{`11457 ${t("miles")}`}</span>
-            <span className="AWD">AWD 4-Cylinder Turbo</span>
+            <h5 className="carTitle">Mercedes C-200 2020</h5>
+            <span className="miles">{`12349 ${t("kilometer")}`}</span>
+            <span className="AWD">136 hp Turbo</span>
 
             <div className="price_seller flex">
-              <span className="price">$41,200</span>
-              <span className="seller">{t("bestSeller")}</span>
+              <span className="price"><CiDiscount1 /></span>
+              <span className="seller"><FaStar /></span>
             </div>
           </div>
 
@@ -58,15 +60,15 @@ const Trending = (props: Props) => {
               data-aos-duration="3000"
               className="imgDiv"
             >
-              <img src="assets/img/carModels/bmw320-kırmızı.png" alt="carImage" />
+              <img src="assets/img/carModels/mercedesc180-siyah.png" alt="carImage" />
             </div>
-            <h5 className="carTitle">Used 2019 Audi S4 Premium Plus</h5>
-            <span className="miles">{`11457 ${t("miles")}`}</span>
-            <span className="AWD">AWD 4-Cylinder Turbo</span>
+            <h5 className="carTitle">Mercedes C-180 2015</h5>
+            <span className="miles">{`63403  ${t("kilometer")}`}</span>
+            <span className="AWD">AWD 156 hp Turbo</span>
 
             <div className="price_seller flex">
-              <span className="price">$41,200</span>
-              <span className="seller">{t("bestSeller")}</span>
+              <span className="price"><CiDiscount1 /></span>
+              <span className="seller"><FaStar /></span>
             </div>
           </div>
         </div>
