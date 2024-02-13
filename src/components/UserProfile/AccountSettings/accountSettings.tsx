a3938ca682169211229b27e6aea5b583c7b31399
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-13 16:07:06 +0300

    created account settings page in user sidebar

--- src/components/UserProfile/AccountSettings/accountSettings.tsx
+++ src/components/UserProfile/AccountSettings/accountSettings.tsx
@@ -3,7 +3,24 @@ import './AccountSettings.css'
 
 const AccountSettings = () => {
   return (
-    <div>accountSettings</div>
+    <div className='accountSettings'>
+      <h2 className='mainHead1'>Personal Information</h2>
+      <div className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Name <span>*</span></label>
+          <input type='text' name='name' id='name' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='name'>Phone <span>*</span></label>
+          <input type='text' name='phone' id='phone' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='name'>Email <span>*</span></label>
+          <input type='email' name='email' id='email' />
+        </div>
+      </div>
+      <button className='mainButton1'>Save Changes</button>
+    </div>
   )
 }
 
