2c5b0d8067d4b8d1955416f2d1d2ec40233f3719
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-13 03:46:22 +0300

    added account setting and your reservations to user sidebar

--- src/components/UserProfile/UserSidebar/userSidebar.tsx
+++ src/components/UserProfile/UserSidebar/userSidebar.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './UserSidebar.css'
+import { VscAccount } from "react-icons/vsc";
+import { IoBagCheckOutline } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 interface UserSidebarProps {
   activepage: any
@@ -9,8 +12,37 @@ const UserSidebar: React.FC<UserSidebarProps> = ({activepage}) => {
   return (
     <div className='userSidebar'>
       {
-        activepage === 'account-settings' ? <div className='s1'></div> : <div className='s2'></div>
+        activepage === 'account-settings' 
+        ? 
+        <div className='s2'>
+          <VscAccount className='img'/>
+          <span>Account Settings</span>
+        </div> 
+        : 
+        <Link to="/profile/account-settings">
+          <div className='s1'>
+            <VscAccount className='img'/>
+            <span>Account Settings</span>
+          </div>
+        </Link>
       }
+
+      {
+        activepage === 'your-reservations' 
+        ? 
+        <div className='s2'>
+          <IoBagCheckOutline className='img'/>
+          <span>Your Reservations</span>
+        </div> 
+        : 
+        <Link to="/profile/your-reservations">
+          <div className='s1'>
+            <IoBagCheckOutline className='img'/>
+            <span>Your Reservations</span>
+          </div>
+        </Link>
+      }
+
     </div>
   )
 }
