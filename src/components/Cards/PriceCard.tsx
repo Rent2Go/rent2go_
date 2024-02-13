3382afc1fb1b90e1fb6484af572720556603ae0c
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-13 04:15:12 +0300

    created price Component

--- src/components/Cards/PriceCard.tsx
+++ src/components/Cards/PriceCard.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { CarModel } from '../../models/responses/cars/GetCar';
 
-type Props = { }
+type Props = {cars:CarModel | undefined }
 
 const PriceCard = (props: Props) => {
+ 
   return (
-    <div>PriceCard</div>
+    <div className='priceCard'>
+     <p><span><b>Unit Price : </b></span>₺ {props.cars?.dailyPrice},00</p> 
+     <p><span><b>Discount : </b></span>% 10 - ₺ 289,00</p> 
+     <p><span><b>Tax : </b></span>% 8 - ₺ 231,2</p> 
+     <p><span><b>Total Price : </b></span>₺ 19.824,00</p> 
+    </div>
   )
 }
 
