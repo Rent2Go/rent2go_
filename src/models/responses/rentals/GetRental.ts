1f550c1571ffef2baca7c1a3da067ddaf5af697f
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-14 01:57:51 +0300

    updated RentalModel in GetRental.ts

--- src/models/responses/rentals/GetRental.ts
+++ src/models/responses/rentals/GetRental.ts
@@ -1,46 +1,39 @@
 export interface RentalModel {
-  id: number;
-  startDate: string;
-  endDate: string;
-  returnDate: string;
-  startKilometer: number;
-  endKilometer: number;
-  totalPrice: number;
-  discountPercentage: number;
-  car: {
     id: number;
-    kilometer: number;
-    year: number;
-    dailyPrice: number;
-    plate: string;
-    imageUrl: string;
-    colorName: string;
-    bodyType: string;
-    fuelType: string;
-    gearType: string;
-    cylinderCount: string;
-    enginePower: string;
-  };
-  employee: {
-    id: number;
-    name: string;
-    surname: string;
-    phoneNumber: string;
-    email: string;
-    city: string;
-    district: string;
-    address: string;
-    salary: number;
-  };
-  customer: {
-    id: number;
-    name: string;
-    surname: string;
-    phoneNumber: string;
-    email: string;
-    nationalityId: string;
-    city: string;
-    district: string;
-    address: string;
-  };
+    startDate: string;
+    endDate: string;
+    returnDate: string;
+    startKilometer: number;
+    endKilometer: number;
+    totalPrice: number;
+    car: {
+      id: number;
+      kilometer: number;
+      year: number;
+      dailyPrice: number;
+      plate: string;
+      imageUrl: string;
+      model: {
+        id: number;
+        name: string;
+        brand: {
+          id: number;
+          name: string;
+        };
+      };
+      color: {
+        id: number;
+        name: string;
+        hexCode: string;
+      };
+      bodyType: string;
+      fuelType: string;
+      gearType: string;
+      cylinderCount: string;
+      enginePower: string;
+      active: boolean;
+    };
+    customerId: number;
+    employeeId: number;
+    discountPercentage: number;
 }
