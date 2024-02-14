0973a80d8c92a64a638dfd20aab08b75ad59dcb8
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-14 18:01:04 +0300

    created payment methods routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import { CarPage, HomePage, Login, Register, BrandPage, OurTeamPage, ContactPage, PrivacyPolicy, ReservationPage, CookiePolicy, TermsOfUse, ProfilePage, PaymentPage, NotFoundPage } from "./pages";
+import {
+  CarPage,
+  HomePage,
+  Login,
+  Register,
+  BrandPage,
+  OurTeamPage,
+  ContactPage,
+  PrivacyPolicy,
+  ReservationPage,
+  CookiePolicy,
+  TermsOfUse,
+  ProfilePage,
+  NotFoundPage,
+  Online,
+  BankTransfer,
+  Cash,
+} from "./pages";
 
 import "./App.css";
 import { RentalModel } from "./models/responses/rentals/GetRental";
@@ -12,54 +29,60 @@ import ChangePassword from "./pages/ChangePasswordPage/ChangePassword";
 import { useDispatch, useSelector } from "react-redux";
 import { setPageSettings } from "./store/slices/settingsSlice";
 import SettingsService from "./services/SettingsService";
-import {Helmet} from 'react-helmet'
+import { Helmet } from "react-helmet";
+import { PaymentProvider } from "./contexts/PaymentContext";
 
 function App() {
-
-  const settings = useSelector((state:any) => state.settings.setting);
+  const settings = useSelector((state: any) => state.settings.setting);
   const dispatch = useDispatch();
-  const handleSetPageSettings = async() => {
-  const response =     await SettingsService.getById(1)
+  const handleSetPageSettings = async () => {
+    const response = await SettingsService.getById(1);
     dispatch(setPageSettings(response.data.data));
   };
 
 
   useEffect(() => {
-    handleSetPageSettings()
-  }, [])
-  
+    handleSetPageSettings();
+  }, []);
 
+  if (!settings) return <div>Şeyhmus will be modify here .. :)</div>;
 
-  if(!settings) return <div>asdasd</div>
-  
   return (
     <>
-     <Helmet>
-      <title>{settings.title}</title>
-      <link rel="icon" href={ settings.logo}  />
-    </Helmet> 
+      <Helmet>
+        <title>{settings.title}</title>
+        <link rel="icon" href={settings.logo} />
+      </Helmet>
       <main>
-      <OverlayLoader/>
-        <Routes>
-          <Route path="/"  element={<HomePage/>}  />
-          <Route path="/sign-in" element={<Login />} />
-          <Route path="/sign-in/reset-password" element={<ResetPassword />} />
-          <Route path="/sign-in/change-password" element={<ChangePassword />} />
-          <Route path="/sign-up" element={<Register />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/cars" element={<CarPage />} />
-          <Route path="/brands" element={<BrandPage />} />
-          <Route path="/our-team" element={<OurTeamPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/reservation/:id" element={<ReservationPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/cookie-policy" element={<CookiePolicy />} />
-          <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/terms-of-use" element={<TermsOfUse />} />
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        
-        </Routes>
+        <OverlayLoader />
+        <PaymentProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sign-in" element={<Login />} />
+            <Route path="/sign-in/reset-password" element={<ResetPassword />} />
+            <Route
+              path="/sign-in/change-password"
+              element={<ChangePassword />}
+            />
+            <Route path="/sign-up" element={<Register />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/cars" element={<CarPage />} />
+            <Route path="/brands" element={<BrandPage />} />
+            <Route path="/our-team" element={<OurTeamPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/cookie-policy" element={<CookiePolicy />} />
+
+            <Route path="/reservation/:id" element={<ReservationPage />} />
+            <Route path="/payment/cash" element={<Cash />} />
+            <Route path="/payment/online" element={<Online />} />
+            <Route path="/payment/bankTransfer" element={<BankTransfer />} />
+
+            <Route path="/terms-of-use" element={<TermsOfUse />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </PaymentProvider>
       </main>
     </>
   );
