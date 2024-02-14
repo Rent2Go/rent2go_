00f172804335b09ffa39624d86351896f7705536
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-14 23:24:07 +0300

    added translation to accountSettings component

--- src/components/UserProfile/AccountSettings/accountSettings.tsx
+++ src/components/UserProfile/AccountSettings/accountSettings.tsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import './accountSettings.css'
+import { useTranslation } from "react-i18next";
 
 const AccountSettings = () => {
+  const { t } = useTranslation();
+
   return (
     <div className='accountSettings'>
-      <h2 className='mainHead1'>Personal Information</h2>
+      <h2 className='mainHead1'>{t("personalInformations")}</h2>
       <div className='form'>
         <div className='form-group'>
-          <label htmlFor='name'>Name <span>*</span></label>
+          <label htmlFor='name'>{t("firstName")} <span>*</span></label>
           <input type='text' name='name' id='name' />
         </div>
         <div className='form-group'>
-          <label htmlFor='name'>Surname <span>*</span></label>
+          <label htmlFor='name'>{t("lastName")} <span>*</span></label>
           <input type='text' name='surname' id='surname' />
         </div>
         <div className='form-group'>
-          <label htmlFor='name'>Phone <span>*</span></label>
+          <label htmlFor='name'>{t("phone")} <span>*</span></label>
           <input type='text' name='phone' id='phone' />
         </div>
         <div className='form-group'>
-          <label htmlFor='name'>Email <span>*</span></label>
+          <label htmlFor='name'>{t("email")} <span>*</span></label>
           <input type='email' name='email' id='email' />
         </div>
       </div>
-      <button className='mainButton1'>Save Changes</button>
+      <button className='mainButton1'>{t("saveChanges")}</button>
     </div>
   )
 }
