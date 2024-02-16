0bd2e22266e8ab0d6380e9df6150a71f3c420a3e
Yagmur Curku <yagmurcurku@gmail.com>
2024-02-17 01:45:50 +0300

    Added function to retrieve cities and districts from database

--- src/components/UserProfile/UserLocation/UserLocation.tsx
+++ src/components/UserProfile/UserLocation/UserLocation.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./UserLocation.css";
 import { useTranslation } from "react-i18next";
 import * as Yup from "yup";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import FormikInput from "../../FormikInput/FormikInput";
+import DistrictService from "../../../services/DistrictService";
+import { CityModel } from "../../../models/responses/cities/GetCity";
+import { DistrictModel } from "../../../models/responses/districts/GetDistrict";
+import CityService from "../../../services/CityService";
+import { onChange } from "react-toastify/dist/core/store";
 
 const UserLocation = () => {
+
   const { t } = useTranslation();
+
+    
+  const [cities, setCities] = useState<CityModel[]>([]);
+  const [districts, setDistricts] = useState<DistrictModel[]>([]);
+  const [cityId, setCityId] = useState<number>(0)
+  const [selectedFilter, setSelectedFilter] = useState<DistrictModel[]>([])
+
+
 
   const initialValues: any = {
     city: "",
@@ -22,6 +36,41 @@ const UserLocation = () => {
 
   const handleSubmit = () => {};
 
+  
+  const getCities = async () => {
+    await CityService.getAll()
+    .then((res) => {
+      setCities(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const getDistricts = async () => {
+    await DistrictService.getAll()
+    .then((res) => {
+      setDistricts(res.data.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    getCities()
+    getDistricts()
+  },[])
+
+  const handleCityChange = async (selectedCity: number) => {
+    setCityId(selectedCity);
+    const city = cities.find((city) => city.id == cityId);
+
+    const selectedCityDistrict = districts.filter((district: DistrictModel) => district.city.id == city?.id)
+    setSelectedFilter(selectedCityDistrict)
+  }
+
+
   return (
     <div className="userLocation">
       <h2 className="mainHead1 text-center">{t("locationInformation")}</h2>
@@ -34,18 +83,32 @@ const UserLocation = () => {
             <Form>
                 <div className="row">
                     <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
-                        <FormikInput
-                            name="city"
-                            type="text"
-                            label={t("city")}
-                        ></FormikInput>
+                      <div>
+                        <label className="form-label">City</label>
+                        <Field
+                          name="city"
+                          as="select"
+                          className="form-control"
+                          onChange={(e: any) => handleCityChange(e.target.value)}
+                          value={cityId}
+                        >
+                        {cities.map((value:any) => (
+                          <option key={value.id} value={value.id}>{value.name.toUpperCase()}</option>
+                        ))}
+                        </Field>
+                      </div>
                     </div>
                     <div className="col-xl-6 col-l-6 col-md-12 col-sm-12">
-                        <FormikInput
-                            name="district"
-                            type="text"
-                            label={t("district")}
-                        ></FormikInput>
+                        <label className="form-label">District</label>
+                        <Field
+                          name="districtId"
+                          as="select"
+                          className="form-control"
+                        >
+                        {selectedFilter.map((value: DistrictModel) => (
+                          <option key={value.id} value={value.id}>{value.districtName.toUpperCase()}</option>
+                        ))}
+                        </Field>
                     </div>
                 </div>
                 <div className="row">
