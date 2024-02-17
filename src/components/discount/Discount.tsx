d896320da038e92de3b08359a66b3987d290b73b
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-18 00:30:08 +0300

    modified discount code & reservation page styles

--- src/components/discount/Discount.tsx
+++ src/components/discount/Discount.tsx
@@ -1,59 +1,65 @@
-import React, { useState } from 'react';
-import './style.css'
-import DiscountService from '../../services/DiscountService';
-import { DiscountModel } from '../../models/responses/discounts/DiscountModel';
-import { ToastContainer, toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
 
-interface DiscountCodeProps {
+import DiscountService from "../../services/DiscountService";
+import { DiscountModel } from "../../models/responses/discounts/DiscountModel";
+import { ToastContainer, toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { FiPlus } from "react-icons/fi";
+import { FaMinus } from "react-icons/fa";
 
-}
+import "./discount.css";
+interface DiscountCodeProps {}
 
-const DiscountCode: React.FC<DiscountCodeProps> = (props:DiscountCodeProps) => {
+const DiscountCode: React.FC<DiscountCodeProps> = (
+  props: DiscountCodeProps
+) => {
   const [discountCode, setDiscountCode] = useState<DiscountModel>();
-  const [inputCode, setInputCode] = useState('');
-  const dispatch = useDispatch()
+  const [inputCode, setInputCode] = useState("");
+  const dispatch = useDispatch();
   console.log(discountCode);
-  
-  const handleApplyDiscount = (e:any) => {
-   setInputCode(e.target.value);
+
+  const handleApplyDiscount = (e: any) => {
+    setInputCode(e.target.value);
   };
 
-  
-
-  const getByDiscountCode = async(discountCode: string) => {
+  const getByDiscountCode = async (discountCode: string) => {
     await DiscountService.getByDiscountCode(discountCode)
       .then((response) => {
         if (response.data.data) {
-          if (!response.data.data.isActive ) {
-            throw new Error('Discount is not active');
+          if (!response.data.data.isActive) {
+            throw new Error("Discount is not active");
           }
           setDiscountCode(response.data.data);
-          toast.success(response.data.data.discountCode)
+          toast.success(response.data.data.discountCode);
         }
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error("Error:", error);
       });
-  }
-
+  };
 
   return (
-    <div className="discount-code">
-      <label htmlFor="discount-code-input">Enter your discount code:</label>
+    <div className="discountContainer">
       <input
         type="text"
         id="discount-code-input"
+        className="form-control"
         name="discount-code"
         value={inputCode}
+        placeholder="Enter Discount"
         onChange={(e) => handleApplyDiscount(e)}
       />
-      <button onClick={()=> getByDiscountCode(inputCode)} >
-        Apply
+      <button type="button" title="." className="btn btn-sm btn-apply" onClick={() => getByDiscountCode(inputCode)}>
+      <FiPlus />
+
       </button>
-      <ToastContainer position='bottom-center' />
+      <button type="button" title="." className="btn btn-sm btn-cancel" onClick={() => getByDiscountCode(inputCode)}>
+      <FaMinus />
+
+
+      </button>
+      <ToastContainer position="bottom-center" />
     </div>
-    
   );
 };
 
--- src/pages/ReservationPage/ReservationPage.tsx
+++ src/pages/ReservationPage/ReservationPage.tsx
@@ -168,6 +168,7 @@ const ReservationPage = () => {
                 <CustomerCard />
               </div>
               <div className="dateContentContainer">
+             
                 <p>
                   
                     <b>Start Date : </b> {rentStartDate.toDateString()}
@@ -178,6 +179,7 @@ const ReservationPage = () => {
                     <b>End Date : </b> {rentEndDate.toDateString()}
                   </span>
                 </p>
+                <DiscountCode  />
               </div>
             </div>
             <div className="middleContent">
@@ -287,7 +289,7 @@ const ReservationPage = () => {
                   </div>
                   <div className="btnContainer">
                     <button
-                      className="btn btn-secondary btn-sm"
+                      className="btn btn-apply btn-sm"
                       type="submit"
                       title="Send"
                       onClick={isAuthenticated}
@@ -296,7 +298,7 @@ const ReservationPage = () => {
                     </button>
                     <Link
                       to="/cars"
-                      className="btn btn-dark btn-sm"
+                      className="btn btn-cancel btn-sm"
                       title="Send"
                     >
                       Cancel
@@ -304,7 +306,7 @@ const ReservationPage = () => {
                   </div>
                 </div>
               </form>
-              <DiscountCode  />
+              
             </div>
             <div className="noteContainer"></div>
           </div>
