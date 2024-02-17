3b351a89bfacc7748cb6d2329873456a5827d55a
ShmsErl <erol.sehmus1@gmail.com>
2024-02-17 03:53:27 +0300

    Updated codes.

--- src/components/discount/Discount.tsx
+++ src/components/discount/Discount.tsx
@@ -1,17 +1,42 @@
 import React, { useState } from 'react';
 import './style.css'
+import DiscountService from '../../services/DiscountService';
+import { DiscountModel } from '../../models/responses/discounts/DiscountModel';
+import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 
 interface DiscountCodeProps {
-  onApplyDiscount: (discountCode: string) => void;
+
 }
 
-const DiscountCode: React.FC<DiscountCodeProps> = ({ onApplyDiscount }) => {
-  const [discountCode, setDiscountCode] = useState('');
-
-  const handleApplyDiscount = () => {
-    onApplyDiscount(discountCode);
-    setDiscountCode('');
+const DiscountCode: React.FC<DiscountCodeProps> = (props:DiscountCodeProps) => {
+  const [discountCode, setDiscountCode] = useState<DiscountModel>();
+  const [inputCode, setInputCode] = useState('');
+  const dispatch = useDispatch()
+  console.log(discountCode);
+  
+  const handleApplyDiscount = (e:any) => {
+   setInputCode(e.target.value);
   };
+
+  
+
+  const getByDiscountCode = async(discountCode: string) => {
+    await DiscountService.getByDiscountCode(discountCode)
+      .then((response) => {
+        if (response.data.data) {
+          if (!response.data.data.isActive ) {
+            throw new Error('Discount is not active');
+          }
+          setDiscountCode(response.data.data);
+          toast.success(response.data.data.discountCode)
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
 
   return (
     <div className="discount-code">
@@ -20,13 +45,15 @@ const DiscountCode: React.FC<DiscountCodeProps> = ({ onApplyDiscount }) => {
         type="text"
         id="discount-code-input"
         name="discount-code"
-        value={discountCode}
-        onChange={(e) => setDiscountCode(e.target.value)}
+        value={inputCode}
+        onChange={(e) => handleApplyDiscount(e)}
       />
-      <button onClick={handleApplyDiscount} disabled={!discountCode}>
+      <button onClick={()=> getByDiscountCode(inputCode)} >
         Apply
       </button>
+      <ToastContainer position='bottom-center' />
     </div>
+    
   );
 };
 
