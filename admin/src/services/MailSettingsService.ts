dadcc717cf933747547ad1afd14e70b444ff9847
Soner <syln.soner@gmail.com>
2024-02-18 16:13:03 +0300

    Add MailSettingsService class with getById and updateMailSettings methods

--- admin/src/services/MailSettingsService.ts
+++ admin/src/services/MailSettingsService.ts
@@ -1,1 +1,17 @@
-export{}
+import { UpdateMailSettingsRequest } from "../models/requests/mailSettings/UpdateMailSettingsRequest";
+import { MailSettingsModel } from "../models/responses/mailSettings/GetMailSettingsModel";
+import axiosInstance from "../utils/axiosInsterceptors";
+
+
+class MailSettingService {
+    getById () {
+        return axiosInstance.get<MailSettingsModel>(`mail-configuration`)
+    }
+
+    updateMailSettings (mailSettings:UpdateMailSettingsRequest) {
+        return axiosInstance.put("mail-configuration", mailSettings)
+    }
+    
+}
+
+export default new MailSettingService();
