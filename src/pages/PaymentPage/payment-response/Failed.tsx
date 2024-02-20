2dc23a598222a8614463fe9572ad19d21eeb82ab
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-20 18:45:30 +0300

    complted payment failed page

--- src/pages/PaymentPage/payment-response/Failed.tsx
+++ src/pages/PaymentPage/payment-response/Failed.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Footer, Navbar } from '../../../components'
 
 type Props = {}
 
 const Failed = (props: Props) => {
   return (
-    <div>Failed</div>
+    <>
+    <Navbar />
+    <div className="success">
+      <div className="secContainer shadow-rounded-box">
+        <div className="titleContainer">
+          <h2 className='text-alert'>Payment Failed !</h2>
+        </div>
+        <div className="contentContainer">
+          <p>
+            <span>Your payment has been failed.. </span><br></br>
+            <span>
+              Your reservation request has not been received..
+            </span>
+            <span>
+            You can contact your bank or you can try another card/account..
+            </span>
+          </p>
+        </div>
+        <div className="actionContainer">
+          <Link
+            to="/cars"
+            type="button"
+            className="btn btn-profile"
+          >
+            Back
+          </Link>
+          <Link to="/" type="button" className="btn btn-home">
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </>
   )
 }
 
