dbc192d013a1c34bd390d12a29f1354b3ae184d1
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-20 17:51:47 +0300

    complated payment-success page

--- src/pages/PaymentPage/payment-response/Successful.tsx
+++ src/pages/PaymentPage/payment-response/Successful.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
-
-type Props = {}
+import React from "react";
+import { Footer, Navbar } from "../../../components";
+import { Link } from "react-router-dom";
+import "../payment.css";
+type Props = {};
 
 const Successful = (props: Props) => {
   return (
-    <div>Successful</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="success">
+        <div className="secContainer shadow-rounded-box">
+          <div className="titleContainer">
+            <h2>Payment Successful !</h2>
+          </div>
+          <div className="contentContainer">
+            <p>
+              <span>Your payment has been successfully.. </span><br></br>
+              <span>
+                Your reservation request has been received. Please check your
+                mailbox.
+              </span>
+              <span>
+                You can view your reservation request from your profile page.
+              </span>
+            </p>
+          </div>
+          <div className="actionContainer">
+            <Link
+              to="/profile/your-reservation"
+              type="button"
+              className="btn btn-profile"
+            >
+              Profile
+            </Link>
+            <Link to="/" type="button" className="btn btn-home">
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Successful
+export default Successful;
