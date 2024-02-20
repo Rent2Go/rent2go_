bcf660782d806a68b3c4787e9cbeec1548505426
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-02-21 01:50:40 +0300

    inserted file input for profile img

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import { AccountSettings, UserSidebar } from '../../components'
+import { AccountSettings, UserSidebar } from "../../components";
 
-import './profilePage.css'
+import "./profilePage.css";
 
-import YourReservations from '../../components/UserProfile/YourReservations/YourReservations'
-import ChangePassword from '../../components/UserProfile/ChangePassword/ChangePassword'
-import DriversLicense from '../../components/UserProfile/DriversLicense/DriversLicense'
-import UserLocation from '../../components/UserProfile/UserLocation/UserLocation'
-import Disclaimer from '../../components/UserProfile/Disclaimer/Disclaimer'
-import { Helmet } from 'react-helmet'
-import { useSelector } from 'react-redux'
-import './profilePage.css'
-import { Footer, Navbar } from '../../components'
+import YourReservations from "../../components/UserProfile/YourReservations/YourReservations";
+import ChangePassword from "../../components/UserProfile/ChangePassword/ChangePassword";
+import DriversLicense from "../../components/UserProfile/DriversLicense/DriversLicense";
+import UserLocation from "../../components/UserProfile/UserLocation/UserLocation";
+import Disclaimer from "../../components/UserProfile/Disclaimer/Disclaimer";
+import { Helmet } from "react-helmet";
+import { useSelector } from "react-redux";
+import "./profilePage.css";
+import { Footer, Navbar } from "../../components";
 
-
-
-
-type Props = {}
+type Props = {};
 
 const ProfilePage = (props: Props) => {
   const settings = useSelector((state: any) => state.settings.setting);
@@ -27,31 +24,29 @@ const ProfilePage = (props: Props) => {
 
   return (
     <>
-
-      <Helmet >
+      <Helmet>
         <title>{settings.title} - Profile </title>
-
       </Helmet>
 
-      <div className='userProfile'>
+      <div className="userProfile">
         <Navbar />
-        <div className='userProfileIn'>
-          <div className='left'>
+        <div className="userProfileIn">
+          <div className="left">
             <UserSidebar activepage={activepage} />
           </div>
-          <div className='right'>
-            {activepage === 'account-settings' && <AccountSettings />}
-            {activepage === 'your-reservations' && <YourReservations />}
-            {activepage === 'change-password' && <ChangePassword />}
-            {activepage === 'drivers-license' && <DriversLicense />}
-            {activepage === 'location-settings' && <UserLocation />}
-            {activepage === 'disclaimer' && <Disclaimer />}
+          <div className="right">
+            {activepage === "account-settings" && <AccountSettings />}
+            {activepage === "your-reservations" && <YourReservations />}
+            {activepage === "change-password" && <ChangePassword />}
+            {activepage === "drivers-license" && <DriversLicense />}
+            {activepage === "location-settings" && <UserLocation />}
+            {activepage === "disclaimer" && <Disclaimer />}
           </div>
         </div>
         <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
