97a3282d5330bdf60bc7bfe72303b69b1ed0a2fa
ShmsErl <erol.sehmus1@gmail.com>
2024-02-21 00:11:20 +0300

    After the confirmation of the payment transaction, the necessary api request was created to send an e-mail request to the user.

---
+++ src/models/mail/MailInfıModel.ts
@@ -0,0 +1,13 @@
+export interface MailInfoModel {
+    name: string;
+    email: string;
+    phone: string;
+    startDate: Date;
+    endDate: Date;
+    totalDay: number;
+    plate: string;
+    carInfo: string;
+    totalPrice: number;
+
+
+}
